765593667e56c99b6a7ababa45f88efb6a1e114e
ByAlexCod <nocutoday@gmail.com>
2017-11-20 11:16:51 +0100

    ...

--- MATe.Services/Service.cs
+++ MATe.Services/Service.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MATeUI;
+using MATeV2;
+using System.IO;
 
 namespace MATe.Services
 {
     public class Service
     {
+        public static Person Login(string password, string mail)
+        {
+            // only for boss for now
+            if (Context.GetContext().Login(mail, password) != null)
+            {
+                //MultiThreading
+                return Context.GetContext().Login(mail, password);
+            }
+            else return null;
+            
+
+        }
     }
 }
