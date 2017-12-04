a6dff2b75add3deb2d99b4202c4ad8fceb0aa82c
sirine <sassi@intechinfo.fr>
2017-12-04 13:51:46 +0100

    ChangeCount.cs

--- MATeUI/ChangeCount.cs
+++ MATeUI/ChangeCount.cs
@@ -11,7 +11,7 @@ using MATeV2;
 namespace MATeUI
 {
     public partial class ChangeCount : Form
-    {
+    {        
         public ChangeCount(Person user)
         {
             InitializeComponent();
@@ -21,7 +21,7 @@ namespace MATeUI
         private void changeCount21_Load(object sender, EventArgs e)
         {
             changeCount21.ChangeButton += new ChangeCount2.ButtonClickedEventHandler(ChangeCount22);
-            changeCount21.txtName.Text = Authentification.CurrentCtxUser.CurrentUser.Firstname;
+            changeCount21.txtName.Text = Authentification.CurrentCtxUser.CurrentUser.Firstname; ;
             changeCount21.txtLastName.Text = Authentification.CurrentCtxUser.CurrentUser.Lastname;
             changeCount21.txtMail.Text = Authentification.CurrentCtxUser.CurrentUser.Mail;
         }
@@ -30,40 +30,10 @@ namespace MATeUI
         {
             using (var ct = _ctxuser.ObtainAccessor())
             {
-                Context ctx = ct.Context;
-                int i = 0;
-                foreach (var item in ctx.PersonsDictionary.Values)
-                {
-                    if (item.Mail.Equals(Authentification.CurrentCtxUser.CurrentUser.Mail))
-                    {
-                        if ((changeCount21.txtMail.Text == ctx.PersonsDictionary.Values.ElementAt(i).Mail) && (Authentification.CurrentCtxUser.CurrentUser.Mail != changeCount21.txtMail.Text))
-                        {
-                            MessageBox.Show("this mail is already exist");
-
-                        }
-                        else
-                        {
-                            ctx.PersonsDictionary.Values.ElementAt(i).Mail = changeCount21.txtMail.Text;
-                            ctx.PersonsDictionary.Values.ElementAt(i).Firstname = changeCount21.txtName.Text;
-                            ctx.PersonsDictionary.Values.ElementAt(i).Lastname = changeCount21.txtLastName.Text;
-                            break;
-                        }
-                    }
-                    i++;
-                }
-
-                if (Authentification.CurrentCtxUser.CurrentUser is Boss)
-                {
-                    ctx.Boss.Mail = changeCount21.txtMail.Text;
-                    ctx.Boss.Firstname = changeCount21.txtName.Text;
-                    ctx.Boss.Lastname = changeCount21.txtLastName.Text;
-                }
-
-                MessageBox.Show("Profile Change");
+                var s = Authentification.CurrentCtxUser.CurrentUser;
+                s.ModifyProfile(changeCount21.txtName.Text, changeCount21.txtLastName.Text, changeCount21.txtMail.Text);
             }
-            
         }
 
-        
     }
 }
