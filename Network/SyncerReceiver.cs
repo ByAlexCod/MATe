49343e96a533146efa650e1c4d9f289dd6689a95
Nestor <nocutoday@gmail.com>
2018-01-03 07:12:02 +0100

    Bug Fix Utilisation contexte

--- Network/SyncerReceiver.cs
+++ Network/SyncerReceiver.cs
@@ -64,46 +64,52 @@ namespace Network
         }
         void End()
         {
-            Context b = (Context)Serialization.Deserialize();
-            DirectoryInfo di = new DirectoryInfo(_tempUnZipped);
-            foreach(var file in di.GetFiles())
-            {
-                file.Delete();
-            }
-            Directory.Delete(_tempUnZipped);
-            Directory.CreateDirectory(_tempUnZipped);
-            ZipFile.ExtractToDirectory(_tempReceiverPath, _tempUnZipped);
-            DirectoryInfo d = new DirectoryInfo(_tempUnZipped);
-            foreach (var a in d.GetFiles("*.MATe"))
-            {
-                ContextAndUserManager ctxuser = new ContextAndUserManager(b.CompanyName, true);
-                ctxuser.Load(a.FullName);
-                if (File.Exists(_contextStoragePath + @"\" + Path.GetFileName(a.FullName)))
+            
+                DirectoryInfo di = new DirectoryInfo(_tempUnZipped);
+                foreach (var file in di.GetFiles())
                 {
-                    ContextAndUserManager before = new ContextAndUserManager(b.CompanyName, true);
-                    before.Load(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
-
-                    using (var beforeuh = before.ObtainAccessor())
-                    using (var after = ctxuser.ObtainAccessor())
+                    file.Delete();
+                }
+                Directory.Delete(_tempUnZipped);
+                Directory.CreateDirectory(_tempUnZipped);
+                ZipFile.ExtractToDirectory(_tempReceiverPath, _tempUnZipped);
+                DirectoryInfo d = new DirectoryInfo(_tempUnZipped);
+            using (var ct = _baseCtxUser.ObtainAccessor())
+            {
+                Context b = ct.Context;
+                foreach (var a in d.GetFiles("*.MATe"))
+                {
+                    ContextAndUserManager ctxuser = new ContextAndUserManager(b.CompanyName, true);
+                    ctxuser.Load(a.FullName);
+                    if (File.Exists(_contextStoragePath + @"\" + Path.GetFileName(a.FullName)))
                     {
-                        Context beforeuuh = beforeuh.Context;
-                        Context aftereuh = after.Context;
+                        ContextAndUserManager before = new ContextAndUserManager(b.CompanyName, true);
+                        before.Load(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
 
-                        if(aftereuh.ModifyDate > beforeuuh.ModifyDate)
+                        using (var beforeuh = before.ObtainAccessor())
+                        using (var after = ctxuser.ObtainAccessor())
                         {
-                            File.Delete(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
-                            a.CopyTo(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
-                            b.Merge(aftereuh);
+                            Context beforeuuh = beforeuh.Context;
+                            Context aftereuh = after.Context;
+
+                            if (aftereuh.ModifyDate > beforeuuh.ModifyDate)
+                            {
+                                File.Delete(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
+                                a.CopyTo(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
+                                b.Merge(aftereuh);
+                            }
                         }
                     }
-                } else
-                {
-                    a.CopyTo(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
+                    else
+                    {
+                        a.CopyTo(_contextStoragePath + @"\" + Path.GetFileName(a.FullName));
+                    }
                 }
+
+
+
+
             }
-            
-
-
 
         }
 
