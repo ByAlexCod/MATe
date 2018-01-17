ae3b95a0ba116522d074a60fe45c8eb92e250e36
Nestor <nocutoday@gmail.com>
2018-01-17 10:39:09 +0100

    Ignore

--- MATeUI/Authentification.cs
+++ MATeUI/Authentification.cs
@@ -59,6 +59,7 @@ namespace MATeUI
         }
         private void ConnexionBtn_Click(object sender, EventArgs e)
         {
+           
             bool newUser = false;
             
             if (userNameTbx.Text.Trim().Equals(""))
