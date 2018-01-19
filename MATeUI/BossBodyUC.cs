3fde051978226b183e87d50d8898559518f4c8a4
io0freedom0oi <thanhluan.tran.vn@gmail.com>
2018-01-19 15:58:05 +0100

    implement RefreshItems by invoke to refresh BossBodyUC after reveive sync

--- MATeUI/BossBodyUC.cs
+++ MATeUI/BossBodyUC.cs
@@ -20,35 +20,41 @@ namespace MATeUI
         Project p = null;
         ContextAndUserManager _ctxuser = Authentification.CurrentCtxUser;
         ICollection<Project> _projects = new List<Project>();
-
+        public delegate void RefreshItem();
+        public RefreshItem myDelegate;
 
         public BossBodyUC()
         {
+            myDelegate = new RefreshItem(RefreshItems);
             Thread a = new Thread(SomethingChanged);
             a.IsBackground = true;
             a.Start();
             InitializeComponent();
         }
 
+        void RefreshItems()
+        {
+            using (var ct = _ctxuser.ObtainAccessor())
+            {
+                Context ctx = ct.Context;
+                projectManagementOnBody._projectListCbx.DataSource = ctx.ProjectsDictionary.Values.ToArray();
+                projectManagementOnBody._projectListCbx.SelectedItem = ctx.ProjectsDictionary.Values.LastOrDefault();
+                detailProjectOnBody._dgEmployees.Rows.Clear();
+
+                foreach (Employee item in ctx.PersonsDictionary.Values)
+                {
+                    detailProjectOnBody._dgEmployees.Rows.Add(item.Firstname, item.Lastname, item.Mail, item.IP);
+                }
+
+            }
+        }
         void SomethingChanged()
         {
             while(true)
             {
                 if(Network.SyncerReceiver._newReceive)
                 {
-                    using (var ct = _ctxuser.ObtainAccessor())
-                    {
-                        Context ctx = ct.Context;
-                        projectManagementOnBody._projectListCbx.DataSource = ctx.ProjectsDictionary.Values.ToArray();
-                        projectManagementOnBody._projectListCbx.SelectedItem = ctx.ProjectsDictionary.Values.LastOrDefault();
-                        detailProjectOnBody._dgEmployees.Rows.Clear();
-
-                        foreach (Employee item in ctx.PersonsDictionary.Values)
-                        {
-                            detailProjectOnBody._dgEmployees.Rows.Add(item.Firstname, item.Lastname, item.Mail, item.IP);
-                        }
-
-                    }
+                    this.Invoke(myDelegate);
                     Network.SyncerReceiver._newReceive = false;
                 }
             }
