965717ea79dfeebbfef5702b1c72b4a6b1342ce4
Unknown <nocutoday@gmail.com>
2018-01-31 14:09:10 +0100

    To tes Sendfile

---
+++ TestReceiverFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Network;
+
+namespace TestReceiverFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey!");
+            ReceiveFile rc = new ReceiveFile();
+            rc.ReceiveTCP(18000);
+        }
+    }
+}
