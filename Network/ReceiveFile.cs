5899d785b58828ff727ddc80473a694be812deb4
Unknown <nocutoday@gmail.com>
2018-01-31 13:58:15 +0100

    idk

---
+++ Network/ReceiveFile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class ReceiveFile
+    {
+        public void ReceiveTCP(int portN)
+        {
+            int BufferSize = 1024;
+            TcpListener Listener = null;
+            try
+            {
+                Listener = new TcpListener(IPAddress.Any, portN);
+                Listener.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            byte[] RecData = new byte[BufferSize];
+            int RecBytes;
+
+            for (; ; )
+            {
+                TcpClient client = null;
+                NetworkStream netstream = null;
+                
+                try
+                {
+
+
+                    if (Listener.Pending())
+                    {
+                        client = Listener.AcceptTcpClient();
+                        netstream = client.GetStream();
+                        int totalrecbytes = 0;
+
+                        string SaveFileName = string.Empty;
+                        SaveFileName = DateTime.Now + ".zip";
+                        if (SaveFileName != string.Empty)
+                        {
+
+                            FileStream Fs = new FileStream(SaveFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                            while ((RecBytes = netstream.Read (RecData, 0, RecData.Length)) > 0)
+                            {
+                                Fs.Write(RecData, 0, RecBytes);
+                                totalrecbytes += RecBytes;
+                            }
+                            Fs.Close();
+                        }
+                        netstream.Close();
+                        client.Close();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //netstream.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Network/SendFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class SendFile
+    {
+        public void SendTCP(string path, string otherIp, Int32 PortN)
+        {
+            byte[] SendingBuffer = null;
+            int BufferSize = 1024;
+    TcpClient client = null;
+            NetworkStream netstream = null;
+            try
+            {
+               
+                client = new TcpClient(otherIp, PortN);
+                netstream = client.GetStream();
+                FileStream Fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                int NoOfPackets = Convert.ToInt32
+             (Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(BufferSize)));
+                int TotalLength = (int)Fs.Length, CurrentPacketLength, counter = 0;
+                for (int i = 0; i < NoOfPackets; i++)
+                {
+                    if (TotalLength > BufferSize)
+                    {
+                        CurrentPacketLength = BufferSize;
+                        TotalLength = TotalLength - CurrentPacketLength;
+                    }
+                    else
+                        CurrentPacketLength = TotalLength;
+                    SendingBuffer = new byte[CurrentPacketLength];
+                    Fs.Read(SendingBuffer, 0, CurrentPacketLength);
+                    netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+                   
+                }
+
+                
+                     Fs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                netstream.Close();
+                client.Close();
+            }
+        }
+    }
+}
